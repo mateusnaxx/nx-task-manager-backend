45f71b90cd90ddcad4cbde77728bd3e000efe59c
mateusnaxx <mateus.naxx@gmail.com>
2025-08-19 17:01:09 -0300

    feat: move CRUD tasks to controller

--- src/controllers/task.controller.js
+++ src/controllers/task.controller.js
@@ -6,7 +6,7 @@ class TaskController {
         this.res = res;
     }
 
-    async getTasks() {
+    async getAll() {
         try {
             const tasks = await TaskModel.find({});
 
@@ -14,10 +14,88 @@ class TaskController {
                 return this.res.status(404).send({ error: "No tasks found" });
             }
 
-        this.res.status(200).send(tasks);
-    } catch (error) {
-        this.res.status(500).send({ error: "Error when searching for tasks" });
+            this.res.status(200).send(tasks);
+        } catch (error) {
+            this.res
+                .status(500)
+                .send({ error: "Error when searching for tasks" });
+        }
     }
+
+    async getById() {
+        try {
+            const taskId = this.req.params.id;
+            const task = await TaskModel.findById(taskId);
+
+            if (!task) {
+                return this.res.status(404).send({ error: "Task not found" });
+            }
+
+            this.res.status(200).send(task);
+        } catch (error) {
+            this.res
+                .status(500)
+                .send({ error: "Error when searching for task" });
+        }
+    }
+
+    async create() {
+        try {
+            const newTask = new TaskModel(this.req.body);
+            await newTask.save();
+
+            this.res.status(201).send(newTask);
+        } catch (error) {
+            this.res.status(500).send({ error: "Error when creating task" });
+        }
+    }
+
+    async update() {
+        try {
+            const taskId = this.req.params.id;
+            const taskData = this.req.body;
+
+            const taskToUpdate = await TaskModel.findById(taskId);
+
+            const allowedUpdates = ["isCompleted"];
+
+            const requestedUpdates = Object.keys(taskData);
+
+            if (!taskToUpdate) {
+                return this.res.status(404).send({ error: "Task not found" });
+            }
+
+            for (const update of requestedUpdates) {
+                if (allowedUpdates.includes(update)) {
+                    taskToUpdate[update] = taskData[update];
+                } else {
+                    return this.res.status(400).send({
+                        error: "one or more inserted fields are not editable!",
+                    });
+                }
+            }
+
+            const updatedTask = await taskToUpdate.save();
+
+            this.res.status(200).send(updatedTask);
+        } catch (error) {
+            this.res.status(500).send({ error: "Error when updating task" });
+        }
+    }
+
+    async delete() {
+        try {
+            const taskId = this.req.params.id;
+            const deletedTask = await TaskModel.findByIdAndDelete(taskId);
+
+            if (!deletedTask) {
+                return this.res.status(404).send({ error: "Task not found" });
+            }
+
+            this.res.status(200).send(deletedTask);
+        } catch (error) {
+            this.res.status(500).send({ error: "Error when deleting task" });
+        }
     }
 }
 
--- src/routes/task.route.js
+++ src/routes/task.route.js
@@ -6,83 +6,23 @@ const taskModel = require("../models/task.model");
 const router = express.Router();
 
 router.get("/", async (req, res) => {
-    return new TaskController(req, res).getTasks();
+    return new TaskController(req, res).getAll();
 });
 
 router.get("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const task = await taskModel.findById(taskId);
-
-        if (!task) {
-            return res.status(404).send({ error: "Task not found" });
-        }
-
-        res.status(200).send(task);
-    } catch (error) {
-        res.status(500).send({ error: "Error when searching for task" });
-    }
+    return new TaskController(req, res).getById();
 });
 
 router.post("/", async (req, res) => {
-    try {
-        const newTask = new taskModel(req.body);
-        await newTask.save();
-
-        res.status(201).send(newTask);
-    } catch (error) {
-        res.status(500).send({ error: "Error when creating task" });
-    }
+    return new TaskController(req, res).create();
 });
 
 router.patch("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const taskData = req.body;
-
-        const taskToUpdate = await taskModel.findById(taskId);
-
-        const allowedUpdates = ["isCompleted"];
-
-        const requestedUpdates = Object.keys(taskData);
-
-        if (!taskToUpdate) {
-            return res.status(404).send({ error: "Task not found" });
-        }
-
-        for (const update of requestedUpdates) {
-            if (allowedUpdates.includes(update)) {
-                taskToUpdate[update] = taskData[update];
-            } else {
-                return res
-                    .status(400)
-                    .send({
-                        error: "one or more inserted fields are not editable!",
-                    });
-            }
-        }
-
-        const updatedTask = await taskToUpdate.save();
-
-        res.status(200).send(updatedTask);
-    } catch (error) {
-        res.status(500).send({ error: "Error when updating task" });
-    }
+    return new TaskController(req, res).update();
 });
 
 router.delete("/:id", async (req, res) => {
-    try {
-        const taskId = req.params.id;
-        const deletedTask = await taskModel.findByIdAndDelete(taskId);
-
-        if (!deletedTask) {
-            return res.status(404).send({ error: "Task not found" });
-        }
-
-        res.status(200).send(deletedTask);
-    } catch (error) {
-        res.status(500).send({ error: "Error when deleting task" });
-    }
+    return new TaskController(req, res).delete();
 });
 
 module.exports = router;
