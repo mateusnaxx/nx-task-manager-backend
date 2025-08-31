bf620d59d0e15e4c38c285582d5340a57b6e4c5b
mateusnaxx <mateus.naxx@gmail.com>
2025-08-31 11:16:38 -0300

    feat: add cors

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const dotenv = require("dotenv");
-const TaskRouter = require("./src/routes/task.route")
+const cors = require("cors");
+
+const TaskRouter = require("./src/routes/task.route");
 
 const connectToDatabase = require("./src/database/mongoose.database");
 
 dotenv.config();
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 const port = 8000;
 
